819a1d0a66ce63c2f4cc2b7397d030220720c578
Elliot Saba <staticfloat@gmail.com>
2021-12-02 07:36:11 +0000

    Allow specification of socket path

--- hooks/load_keys.sh
+++ hooks/load_keys.sh
@@ -2,8 +2,34 @@
 
 set -eou pipefail
 
+# Helper function to kill execution when something goes wrong
+function die() {
+    echo "ERROR: ${1}" >&2
+    if which buildkite-agent >/dev/null 2>/dev/null; then
+        # By default, the annotation context is unique to the message
+        local CONTEXT=$(echo "${1}" | ${SHASUM})
+        if [[ "$#" -gt 1 ]]; then
+            CONTEXT="${2}"
+        fi
+        buildkite-agent annotate --context="${CONTEXT}" --style=error "${1}"
+    fi
+    exit 1
+}
+
+# Check to see if the user has specified where the ssh-agent socket should live
+SSH_AGENT_A_FLAG=""
+if [[ -v "BUILDKITE_PLUGIN_SSH_AGENT_SOCKET" ]]; then
+    # If we're being asked to put an ssh-agent socket somewhere, let's ensure
+    # that the directory exists, and that there's no file there
+    mkdir -p "$(dirname "${BUILDKITE_PLUGIN_SSH_AGENT_SOCKET}")"
+    if [[ -f "${BUILDKITE_PLUGIN_SSH_AGENT_SOCKET}" ]]; then
+        die "Requested ssh-agent socket path already exists!"
+    fi
+    SSH_AGENT_A_FLAG="-a ${BUILDKITE_PLUGIN_SSH_AGENT_SOCKET}"
+fi
+
 # Start up ssh-agent
-eval "$(ssh-agent -s 2>/dev/null)"
+eval "$(ssh-agent ${SSH_AGENT_A_FLAG} -s 2>/dev/null)"
 
 # Load keyfiles off of disk
 IDX=0
